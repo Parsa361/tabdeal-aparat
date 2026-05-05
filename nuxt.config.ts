5adef387920b0feb9c12fdda3ca63da275850f68
Parsa361 <parsafarahany821@gmail.com>
2026-05-05 17:10:14 +0330

    made the app 'rtl'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+  app: {
+    head: {
+      htmlAttrs: {
+        dir: 'rtl',
+        lang: 'fa',
+      },
+    },
+  },
 })
